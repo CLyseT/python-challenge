b30e49d876b10cee890016fa74e68922b0d68f03
CLyseT <carlyse.thomas@gmail.com>
2021-06-16 21:36:17 -0400

    Update codes for PyBank and PyPoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,56 @@
+# Total number of months
+import os      
+import csv     
 
+filename = ('/Users/carlysethomas/Downloads/python-challenge/Resources/budget_data.csv')
+
+with open(filename, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csvheader = next(csvfile)
+
+    lines = len(list(csvreader))
+    
+
+# Net total of "Profit/Losses" over the entire period
+with open(filename, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csvheader = next(csvfile)    
+    
+    total = sum(int(r[1]) for r in csv.reader(csvfile))
+    
+            
+# Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+# Convert the csvfile to a list - defined as total_profits
+with open(filename, 'r') as total_profits:
+    csvreader_2 = csv.reader(total_profits)
+    
+    next(csvreader_2) 
+    profits = []
+    date = []
+    difference = []
+    
+    for row in csvreader_2:
+       profits.append(float(row[1]))
+       date.append(row[0])
+    
+    for i in range(1, len(profits)):
+        difference.append(profits[i] - profits[i-1])
+        average_difference = round(sum(difference)/len(difference))
+
+print("Financial Analysis")
+print("------------------------------")
+print(f'Total Months = {lines}')
+print(f'Net Total = ${total}')
+print(f'Average Revenue Change: ${round(average_difference)}')
+
+
+# The greatest increase in profits (date and amount) over the entire period
+max_difference = max(difference)
+max_difference_date = str(date[difference.index(max(difference))])
+
+# The greatest decrease in profits (date and amount) over the entire period
+min_difference = min(difference)
+min_difference_date = str(date[difference.index(min(difference))])
+
+print(f'Greatest Increase in Revenue: ${max_difference} {max_difference_date}')
+print(f'Greatest Decrease in Revenue: ${min_difference} {min_difference_date}')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,108 @@
+# The total number of votes cast
+import os
+import csv
+
+filename_ = ('/Users/carlysethomas/Downloads/python-challenge/Resources/election_data.csv')
+
+with open (filename_, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csvheader = next(csvfile) 
+
+    total_votes = len(list(csvreader))
+   
+   
+# A complete list of candidates who received votes DONT UNDERSTAND
+list_of_candidates = []
+
+with open (filename_, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csvheader = next(csvfile) 
+
+    for candidates in csvreader:
+        if candidates[2] not in list_of_candidates:
+            list_of_candidates.append(candidates[2])
+
+
+# The percentage of votes each candidate won
+votes_Khan = 0
+votes_Correy = 0
+votes_Li = 0
+votes_Tooley = 0
+
+with open (filename_, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csvheader = next(csvfile) 
+
+    def votes(voter_data):
+        totals_votes = int(voter_data[3])
+    
+    for name in csvreader:
+        if name[2] == "Khan":
+            votes_Khan = (votes_Khan + 1)
+        elif name[2] == "Correy":
+            votes_Correy = (votes_Correy + 1)
+        elif name[2] == "Li":
+            votes_Li = (votes_Li + 1)
+        elif name[2] == "O'Tooley":
+            votes_Tooley = (votes_Tooley + 1)
+
+total_votes_Khan = votes_Khan
+total_votes_Correy = votes_Correy
+total_votes_Li = votes_Li
+total_votes_Tooley = votes_Tooley
+
+percent_Khan = round((votes_Khan / total_votes)*100)
+percent_Correy = round((votes_Correy / total_votes)*100)
+percent_Li = round((votes_Li / total_votes)*100)
+percent_Tooley = round((votes_Tooley / total_votes)*100)
+
+
+# The total number of votes each candidate won
+votes_Khan = 0
+votes_Correy = 0
+votes_Li = 0
+votes_Tooley = 0
+
+with open (filename_, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csvheader = next(csvfile) 
+
+    for name in csvreader:
+        if name[2] == "Khan":
+            votes_Khan = (votes_Khan + 1)
+        elif name[2] == "Correy":
+            votes_Correy = (votes_Correy + 1)
+        elif name[2] == "Li":
+            votes_Li = (votes_Li + 1)
+        elif name[2] == "O'Tooley":
+            votes_Tooley = (votes_Tooley + 1)
+
+total_votes_Khan = votes_Khan
+total_votes_Correy = votes_Correy
+total_votes_Li = votes_Li
+total_votes_Tooley = votes_Tooley
+
+
+# The winner of the election based on popular vote 
+print("Election Results")
+print("--------------------------------------")
+print(f'Total Number Of Votes = {total_votes}')
+print("--------------------------------------")
+print(f'{list_of_candidates[0]}:  Percentage of Total Votes = {percent_Khan}%  Total Votes for Khan = {total_votes_Khan}')
+print(f'{list_of_candidates[1]}:  Percentage of Total Votes = {percent_Correy}% Total Votes for Correy = {total_votes_Correy}')
+print(f'{list_of_candidates[2]}:  Percentage of Total Votes = {percent_Li}% Total Votes for Li = {total_votes_Li}')
+print(f'{list_of_candidates[3]}:  Percentage of Total Votes = {percent_Tooley}% print Total Votes for O\'Tooley = {total_votes_Tooley}')
+print("--------------------------------------")
+percents = []
+percents.append(percent_Khan)
+percents.append(percent_Correy)
+percents.append(percent_Li)
+percents.append(percent_Tooley)
+
+grouped_list = list(zip(list_of_candidates,percents))
+
+for winner in grouped_list:
+    if winner[1] == max(percents):
+        print(f'The Winner Of The Election Was {winner[0]}')
+print("--------------------------------------")
 
